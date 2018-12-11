7493325747240d93bba1a60c338e40cbf3733320
JasonPhui <1586376147@qq.com>
2018-12-11 21:44:39 +0800

    添加注释

--- video/src/main/java/com/ph/video/controller/VideoIndexController.java
+++ video/src/main/java/com/ph/video/controller/VideoIndexController.java
@@ -3,6 +3,10 @@ package com.ph.video.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author JasonPhui
+ * 2018/12/11
+ */
 @Controller
 public class VideoIndexController {
 
